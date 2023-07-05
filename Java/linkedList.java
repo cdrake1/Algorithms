faa9736b25e585e8ed7b39971c044360a425e3cd
Collin <crdrake2003@gmail.com>
2023-07-05 14:39:28 -0400

    linkedlist and node class created.

---
+++ Java/linkedList.java
@@ -0,0 +1,23 @@
+public class linkedList{
+
+    public class Node{
+        private Node head;
+        private Node next;
+        private int data;
+
+        public Node(Node head, Node next, int data){
+            this.head = head;
+            this.data = data;
+            this.next = next;
+        }
+
+        void push(int data){
+
+        }
+
+        void pop(){
+
+        }
+    }
+
+}
