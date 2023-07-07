5f1c1bb344bd8e156142de312a190eae1c807fc4
Collin <crdrake2003@gmail.com>
2023-07-07 10:07:34 -0400

    mostly finished read file and imported it into LL

--- Java/Assignment1/readFile.java
+++ Java/Assignment1/readFile.java
@@ -1,7 +1,27 @@
+/* Created to read lines from file into an array */
 package Assignment1;
 import java.io.*;
 import java.util.*;
 
 public class readFile {
+    public static void main(String[] args)  throws IOException{
+        Scanner getFilePath = new Scanner(System.in);
+        System.out.print("Enter the file path: "); //get file path
+        String filePath = getFilePath.nextLine();
+        BufferedReader reader = new BufferedReader(new FileReader(filePath)); //create a buffer/file reader for the filepath
+        List<String> fileLines = new ArrayList<String>(); 
+        String line = null;
+        try{
+            while((line = reader.readLine()) != null){ 
+                fileLines.add(line);
+            }
+        }
+        //error catching for problems reading file
+        catch(IOException e){
+            System.err.println("There was a problem reading the file");
+        }
+        fileLines.toArray(); //convert list into array
+        reader.close(); //close readers
+    }
     
 }
