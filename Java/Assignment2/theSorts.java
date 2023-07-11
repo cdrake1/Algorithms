181cd0cdbe37b0e27d40d0cd090586f54d943fcb
Collin <crdrake2003@gmail.com>
2023-07-11 10:28:16 -0400

    started organizing

--- Java/Assignment2/theSorts.java
+++ Java/Assignment2/theSorts.java
@@ -1,5 +1,36 @@
 package Assignment2;
 
+import java.io.IOException;
+
 public class theSorts {
-    
+    public static void main(String[] args) {
+        readFile rf = new readFile(); //make an instance of readFile to call
+        try { //try to read text file into string array 
+            String[] textFile = rf.readThrough();
+            selectionSort(textFile);
+            /*
+            insertionSort(textFile);
+            mergeSort(textFile);
+            quickSort(textFile); 
+            */
+            
+            
+        } catch (IOException e) {//throw exception if error reading file
+            System.err.println("There was a problem reading the file");
+        }
+        
+    }
+
+    static void selectionSort(String[] theText){
+
+    }
+    static void insertionSort(String[] theText){
+
+    }
+    static void mergeSort(String[] theText){
+
+    }
+    static void quickSort(String[] theText){
+
+    }
 }
