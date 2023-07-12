ca43e21cc27112b71151a740bcd0cac118a52655
Collin <crdrake2003@gmail.com>
2023-07-12 12:00:51 -0400

    assignment 2 setup and knuth sort

--- Java/Assignment2/theSorts.java
+++ Java/Assignment2/theSorts.java
@@ -1,13 +1,17 @@
 package Assignment2;
 
-import java.io.IOException;
+import java.io.*;
+import java.lang.*;
+import java.util.*;
 
 public class theSorts {
     public static void main(String[] args) {
         readFile rf = new readFile(); //make an instance of readFile to call
         try { //try to read text file into string array 
             String[] textFile = rf.readThrough();
+            knuthShuffle(textFile); //randomly shuffle list of items
             selectionSort(textFile);
+        
             /*
             insertionSort(textFile);
             mergeSort(textFile);
@@ -21,11 +25,24 @@ public class theSorts {
         
     }
 
+    static String[] knuthShuffle(String[] theText){
+        for(int i = 0; i < theText.length; i++){ //iterate through String array
+            int j = (int) (Math.random() * (i + 1)); //generate random number to shuffle from
+            String temp = theText[j]; //declare termp string and shuffle textarray to random generated numbers
+            theText[j] = theText[i];
+            theText[i] = temp;
+        }
+        return theText;
+    }
+
     static void selectionSort(String[] theText){
+        
 
     }
     static void insertionSort(String[] theText){
-
+        for(String i: theText){
+            System.out.println(i);
+        }
     }
     static void mergeSort(String[] theText){
 
