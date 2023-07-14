2da3a41a99e806c2bba3c42f4711fb78e1320db1
Collin <crdrake2003@gmail.com>
2023-07-14 15:16:19 -0400

    selection sort comments and progress

--- Java/Assignment2/selectionSort.java
+++ Java/Assignment2/selectionSort.java
@@ -3,9 +3,14 @@ package Assignment2;
 public class selectionSort {
     public void sort(String[] textArray) {
         
-        for (int i = 0; i < textArray.length; i++) { //read through each line of array to sort
-            String min = textArray[i]; //set minimum to current index
-            if()// compare strings
+        for (int i = 0; i < textArray.length; i++) { //read through each line of array to sort and set minimum to current index
+            String min = textArray[i];
+            for(int j = i + 1; j < textArray.length; j++){ //read through each line after index
+                if(textArray[j].compareTo(min) < 0){// compare strings to find lesser and set new min
+                    min = textArray[j];
+                }
+
+            }
         }
     }
 }
