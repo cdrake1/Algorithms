c358c93d898cb4c9b0aeba7855f53dd02d04d884
Collin <crdrake2003@gmail.com>
2023-07-14 15:26:34 -0400

    selection sort done. need to test

--- Java/Assignment2/selectionSort.java
+++ Java/Assignment2/selectionSort.java
@@ -1,16 +1,19 @@
 package Assignment2;
 
 public class selectionSort {
-    public void sort(String[] textArray) {
+    public String[] sort(String[] textArray) {
         
         for (int i = 0; i < textArray.length; i++) { //read through each line of array to sort and set minimum to current index
-            String min = textArray[i];
+            int min = i;
             for(int j = i + 1; j < textArray.length; j++){ //read through each line after index
-                if(textArray[j].compareTo(min) < 0){// compare strings to find lesser and set new min
-                    min = textArray[j];
+                if(textArray[j].compareTo(textArray[min]) < 0){// compare strings to find lesser and set new min
+                    min = j;
                 }
-
+                String temp = textArray[i];
+                textArray[i] = textArray[min];
+                textArray[min] = temp;
             }
         }
+        return textArray;
     }
 }
