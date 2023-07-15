9fada47707314e1958e16727ba0a1ae9a29fbb8a
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2023-07-15 09:14:40 -0400

    insertion sort not correct. reding through incorrect way and second compared is wrong

--- Java/Assignment2/insertionSort.java
+++ Java/Assignment2/insertionSort.java
@@ -2,9 +2,9 @@ package Assignment2;
 
 public class insertionSort {
     public String[] sort(String[] textArray){
-        for(int i = 0; i < textArray.length - 1; i++){
+        for(int i = 1; i < textArray.length; i++){
             String marker = textArray[i];
-            int j = i + 1;
+            int j = i - 1;
             if(marker.compareTo(textArray[j]) < 0){
                 String temp = textArray[j];
                 textArray[j] = marker;
