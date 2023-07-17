9bfe82b2a17b02cf1d4299131a8c55e0feccb717
Collin <crdrake2003@gmail.com>
2023-07-17 12:16:09 -0400

    insertion sort going well. need to fix some things and add comments

--- Java/Assignment2/insertionSort.java
+++ Java/Assignment2/insertionSort.java
@@ -2,13 +2,14 @@ package Assignment2;
 
 public class insertionSort {
     public String[] sort(String[] textArray){
-        for(int i = 1; i < textArray.length; i++){ //iterate through array
-            String marker = textArray[i]; //set marker to index
-            int j = i - 1;
-            
-            while(){
-                
+        for(int i = 1; i < textArray.length; i++){ //iterate through array starting at 2nd element
+            String temp = textArray[i]; //set temp to index
+            int j = i - 1; //check with previous element
+            while(j > 0 && temp.compareTo(textArray[j]) < 0){
+                textArray[j + 1] = textArray[j]; //switch elements and move through array
+                j = j - 1;
             }
+            textArray[j + 1] = temp;
 
         }
         return textArray;
