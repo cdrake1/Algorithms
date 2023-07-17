d815a73358c4a2d20af077d4f31d8b23dfa81b8a
Collin <crdrake2003@gmail.com>
2023-07-17 12:02:11 -0400

    fixing insertion sort

--- Java/Assignment2/insertionSort.java
+++ Java/Assignment2/insertionSort.java
@@ -2,13 +2,12 @@ package Assignment2;
 
 public class insertionSort {
     public String[] sort(String[] textArray){
-        for(int i = 1; i < textArray.length; i++){
-            String marker = textArray[i];
+        for(int i = 1; i < textArray.length; i++){ //iterate through array
+            String marker = textArray[i]; //set marker to index
             int j = i - 1;
-            if(marker.compareTo(textArray[j]) < 0){
-                String temp = textArray[j];
-                textArray[j] = marker;
-                textArray[i] = temp;
+            
+            while(){
+                
             }
 
         }
