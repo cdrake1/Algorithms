444466e1dde777757affc0f268d009962ff52515
Collin <crdrake2003@gmail.com>
2023-07-18 15:18:55 -0400

    organizing

--- Java/Assignment2/quickSort.java
+++ Java/Assignment2/quickSort.java
@@ -1,8 +1,12 @@
+/* Implementation of quick sort - a sorting algorithm that sorts elements around the arrays pivot */
 package Assignment2;
 
 public class quickSort {
     public String[] sort(String[] textArray){
+        String pivot = textArray[textArray.length]; // create pivot using the last element of the array
+
         return textArray;
     }
     
 }
+ 
