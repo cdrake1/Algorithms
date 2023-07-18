428eeef59461b80774f204bcfc89f687cdd45fc9
Collin <crdrake2003@gmail.com>
2023-07-18 12:00:33 -0400

    first part of merge sort is complet. now need to merge arrays.

--- Java/Assignment2/mergeSort.java
+++ Java/Assignment2/mergeSort.java
@@ -1,13 +1,19 @@
 package Assignment2;
 
+import java.util.Arrays;
+
 public class mergeSort {
-    public String[] sort(String[] textArray, int left, int right){
-        if(){
-            
-        }
-        return textArray;
+    public String[] sort(String[] textArray){
+        int middle = textArray.length / 2;
+
+        String[] left = Arrays.copyOfRange(textArray, 0, middle);
+        String[] right = Arrays.copyOfRange(textArray, middle, textArray.length) ;;
+
+        left = sort(left);
+        right = sort(right);
+        return merge(left, right);
     }
-    void merge(String[] oneHalf, String[] theOther){
+    String[] merge(String[] oneHalf, String[] theOther){
 
     }
     
