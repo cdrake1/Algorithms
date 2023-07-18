b262163bd428e8885169371abd3b58f2e7f66ea6
Collin <crdrake2003@gmail.com>
2023-07-18 12:02:31 -0400

    comments added and spacing fixed

--- Java/Assignment2/mergeSort.java
+++ Java/Assignment2/mergeSort.java
@@ -1,17 +1,16 @@
 package Assignment2;
-
-import java.util.Arrays;
+import java.util.*;
 
 public class mergeSort {
     public String[] sort(String[] textArray){
-        int middle = textArray.length / 2;
+        int middle = textArray.length / 2; //find middle of the array
 
-        String[] left = Arrays.copyOfRange(textArray, 0, middle);
-        String[] right = Arrays.copyOfRange(textArray, middle, textArray.length) ;;
+        String[] left = Arrays.copyOfRange(textArray, 0, middle); //grab the first half
+        String[] right = Arrays.copyOfRange(textArray, middle, textArray.length); //grab the second half
 
-        left = sort(left);
+        left = sort(left); //recursively call sort with left and right halves separately 
         right = sort(right);
-        return merge(left, right);
+        return merge(left, right); //call merge and return solution
     }
     String[] merge(String[] oneHalf, String[] theOther){
 
