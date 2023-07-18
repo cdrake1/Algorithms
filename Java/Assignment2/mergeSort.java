68765dc48d0c219ba4257738f11baa1e5e568597
Collin <crdrake2003@gmail.com>
2023-07-18 11:43:34 -0400

    more progress on merge sort, but nothing much

--- Java/Assignment2/mergeSort.java
+++ Java/Assignment2/mergeSort.java
@@ -1,8 +1,14 @@
 package Assignment2;
 
 public class mergeSort {
-    public String[] sort(String[] textArray){
+    public String[] sort(String[] textArray, int left, int right){
+        if(){
+            
+        }
         return textArray;
+    }
+    void merge(String[] oneHalf, String[] theOther){
+
     }
     
 }
