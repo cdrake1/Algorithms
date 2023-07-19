024c51cc80bb76832c00bcf414c949f02c0f52e2
Collin <crdrake2003@gmail.com>
2023-07-19 11:45:33 -0400

    finished quick sort and commented it. need to test quick and merge

--- Java/Assignment2/quickSort.java
+++ Java/Assignment2/quickSort.java
@@ -2,23 +2,42 @@
 package Assignment2;
 
 public class quickSort {
-    public String[] sort(String[] textArray, int first, int last){
-        if(first < last){
+    public String[] sort(String[] textArray, int first, int last) {
+        if (first < last) {
             int position = partition(textArray, first, last);
-            sort(textArray, first, position - 1); //iterate and sort left side
-            sort(textArray, position + 1, last); //iterate and sort right side
-
+            sort(textArray, first, position - 1); //Iterate and sort the left side
+            sort(textArray, position + 1, last); //Iterate and sort the right side
         }
         return textArray;
     }
 
-    public int partition(String[] array, int first, int last){ //divides the array into parts
+    public int partition(String[] array, int first, int last) {
+        String pivot = array[first]; //Create pivot using the first element of the array
 
-        String pivot = array[first]; // create pivot using the first element of the array
+        int left = first + 1; //Set pointer variables for left and right
+        int right = last;
 
-        
+        while (left <= right) { //Iterate through the array
+            while (left <= right && array[left].compareTo(pivot) <= 0) { //Find an element greater than the pivot
+                left++;
+            }
+            while (left <= right && array[right].compareTo(pivot) > 0) { //Find an element smaller than or equal to the pivot
+                right--;
+            }
+            if (left < right) { //Swap elements and iterate
+                String temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+            }
+        }
 
+        //Move the pivot to its correct position
+        String temp = array[first];
+        array[first] = array[right];
+        array[right] = temp;
+
+        return right;
     }
-    
 }
+
  
