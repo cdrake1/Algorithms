79f2c42c276a95527d82552c6852cdded010462a
Collin <crdrake2003@gmail.com>
2023-07-21 12:21:44 -0400

    starting assignment 3

---
+++ Java/Assignment3/theSearch.java
@@ -0,0 +1,9 @@
+package Assignment3;
+
+public class theSearch {
+    public static void main(String[] args){
+        readFile rf = new readFile(); //make an instance of readFile to call
+        String[] textFile = rf.readThrough(); //try to read text file into string array 
+    }
+    
+}
