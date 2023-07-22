4faeea3fa5e6d453b9cd418b8cfaf8a34fa23774
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2023-07-21 22:23:54 -0400

    succesfully implemented and tested linear search

--- Java/Assignment3/linear.java
+++ Java/Assignment3/linear.java
@@ -4,17 +4,29 @@ package Assignment3;
 import java.util.Random;
 
 public class linear {
-    public int search(String[] sortedMagic, String[] keyItems){
 
-        return 0;
+    private int comparisons = 0; //count comparisons
+
+    public int search(String[] sortedMagic, String[] keyItems){ //linear search for 42 random items. count number of comparisons
+        int itemsFound = 0; //to check if random item is found
+        for(int i = 0; i < keyItems.length; i++){
+            for(int j = 0; j < sortedMagic.length; j++){
+                comparisons++;
+                if(sortedMagic[j].equals(keyItems[i])){ //if item is found break loop
+                    itemsFound++;
+                    break;
+                }
+            }
+        }
+        return comparisons;
     }
 
-    static String[] random(String[] sortedMagic){
+    static String[] random(String[] sortedMagic){ //randomly select 42 items to search for linearly
         Random rand = new Random();
 
         String[] fortyTwoItems = new String[42]; //create new string array of length 42
 
-        for(int i = 0; i < 42; i++){ //randomly select 42 items to search for linearly
+        for(int i = 0; i < 42; i++){
             int randomItem = rand.nextInt(sortedMagic.length); // generate random numbers and add to new array
             fortyTwoItems[i] = sortedMagic[randomItem];
         }
--- Java/Assignment3/theSearch.java
+++ Java/Assignment3/theSearch.java
@@ -6,8 +6,16 @@ public class theSearch {
         readFile rf = new readFile(); //make an instance of readFile to call
         try{
             String[] textFile = rf.readThrough(); //try to read text file into string array 
+
             mergeSort sorted = new mergeSort(); //call merge sort and pass the text file
             String[] sortedFile = sorted.sort(textFile);
+
+            linear linearSearch = new linear(); //create instance of linear class, generate list of 42 random items and call linear search
+            String[] randomItems = linearSearch.random(sortedFile);
+            int numLinear = linearSearch.search(sortedFile, randomItems);
+
+            comparisons(numLinear);
+
 
         }catch (IOException e) {//throw exception if error reading file
             System.err.println("There was a problem reading the file");
@@ -19,6 +27,10 @@ public class theSearch {
             System.out.println(i);
         }
 
+    }
+
+    static void comparisons(int linearComparisons){
+        System.out.println("Linear Comparisons: " + linearComparisons);
     }
     
 }
