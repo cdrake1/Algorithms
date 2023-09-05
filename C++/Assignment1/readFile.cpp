4ac5b81ef89ef37135e26b71ec4f4088348559d1
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2023-09-05 19:24:38 -0400

    read file started

--- C++/Assignment1/readFile.cpp
+++ C++/Assignment1/readFile.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -7,7 +9,20 @@ using namespace std;
 
 class readFile {
     public:
-        readThrough(){
+        readFile(){
+            string filePath;
+            string line;
+            vector<string> fileLines;
+            cout << "Enter the path of the file: ";
+            cin >> filePath; //grab user input for file path
+
+            ifstream magicfile(filePath); //create version of file
+            if(magicfile.is_open()){
+
+            }
+            else{
+                cout << "Something went wrong when trying to open the file";
+            }
 
         }
 
