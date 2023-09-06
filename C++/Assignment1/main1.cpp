5b0dbc60a418314d52a41aa13a88d0cec43a830d
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2023-09-06 11:08:37 -0400

    instructions

--- C++/Assignment1/main1.cpp
+++ C++/Assignment1/main1.cpp
@@ -1,4 +1,5 @@
 //main file to run assignment 1 folder
+//rebuild progam1 and run it by running: clang++ -o program1 main1.cpp readFile.cpp and ./program1 in terminal
 #include "readFile.h"
 
 #include <iostream>
