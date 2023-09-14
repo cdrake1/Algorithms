ab871908a46cc4cab1e6c3455316c839f574ef7f
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2023-09-13 21:06:23 -0400

    fixed node cpp file.  going to run and test

--- C++/Assignment1/Node.cpp
+++ C++/Assignment1/Node.cpp
@@ -7,84 +7,56 @@
 
 using namespace std;
 
-class Node{
-    public:
-        Node* next; //pointer to next node
-        char val; //data within node
-
-        Node(char val){ //constructor for node class
-            this->val = val;
-            this->next = nullptr;
-        }
+Node::Node(char val){ //Node class constructor
+    this->val = val;
+    this->next = nullptr;
 };
 
-class Stack{
-    Node* top; //top of stack
-
-    public:
-        void push(char val){
-            Node* newNode = new Node(val);
-
-            if (isEmpty()) //check if stack empty
-            {
-                top = newNode;
-            }
-            else{ //if not empty make new node top
-                newNode->next = top;
-                top = newNode;
-            }
-        }
-        void pop(){ //remove the top element of the stack
-            if (isEmpty())
-            {
-               throw invalid_argument("Stack is empty");
-            }
-            else{
-                top = top->next;
-            }
-        }
-        bool isEmpty(){ //check if stack is empty
-            if (top == nullptr){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
+void Stack::push(char val){ //push node onto stack
+    Node* newNode = new Node(val);
+    if(isEmpty()){
+        top = newNode;
+    }
+    else{
+        newNode->next = top;
+        top = newNode;
+    }
 };
 
-class Queue{
-    Node* front; //front and back of line/queue
-    Node* rear;
+void Stack::pop(){ //pop top node off stack
+    if (isEmpty()){
+        throw invalid_argument("Stack is empty");
+    }
+    else{
+        top = top->next;
+    }
+};
 
-    public:
-        void enqueue(char val){ //enqueue new node
-            Node* newNode2 = new Node(val);
-            if(isEmpty()){ //if empty make node front and rear
-                front = newNode2;
-                rear = newNode2;
-            }
-            else{ //if not make it rear
-                rear->next = newNode2;
-                rear = newNode2;
-            }
-        }
+bool Stack::isEmpty(){ //check if stack is empty
+    return top == nullptr;
+};
 
-        void dequeue(){ //dequeue first node
-            if(isEmpty){
-                throw invalid_argument("Queue is empty");
-            }
-            else{
-                front = front->next; //change front
-            }
-        }
+void Queue::enqueue(char val){ //add node to back of line
+    Node* newNode2 = new Node(val);
+    if(isEmpty()){ //if empty make node front and rear
+        front = newNode2;
+        rear = newNode2;
+    }
+    else{ //if not make it rear
+        rear->next = newNode2;
+        rear = newNode2;
+    }
+};
 
-        bool isEmpty(){ //check if queue is empty
-            if(front == nullptr){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
+void Queue::dequeue(){ //remove node from front of line
+    if(isEmpty){
+        throw invalid_argument("Queue is empty");
+    }
+    else{
+        front = front->next; //change front
+    }
+};
+
+bool Queue::isEmpty(){ //check if queue is empty
+    return front == nullptr;
 };
