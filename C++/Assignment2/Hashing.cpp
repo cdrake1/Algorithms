8741d8b3df16ea4d7d68d00370d6b6dc1e70d7a5
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2023-10-20 19:26:51 -0400

    comments for cpp file

--- C++/Assignment2/Hashing.cpp
+++ C++/Assignment2/Hashing.cpp
@@ -1,22 +1,22 @@
 //this file creates the hashing and chaining functions for a hashtable
 #include "Hashing.hpp"
 
-HashTable:: HashTable(){
+HashTable:: HashTable(){ //constructor function for hashtable
 
 }
 
-void HashTable:: get(){
+void HashTable:: get(){ //grab and element from hashtable and return it
 
 }
 
-void HashTable:: put(){
+void HashTable:: put(){ //add and element to the hashtable
 
 }
 
-void HashTable:: outputHashTable(){
+void HashTable:: outputHashTable(){ //print out the elements within the hashtable
 
 }
 
-void HashTable:: hashing(){
+void HashTable:: hashing(){ //calculate hash value for given input
 
 }
