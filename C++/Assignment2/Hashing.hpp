649d6389eb0d5110b3441a4d99e336b5afedf647
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2023-10-20 19:12:49 -0400

    setting up hpp file

--- C++/Assignment2/Hashing.hpp
+++ C++/Assignment2/Hashing.hpp
@@ -5,8 +5,16 @@ using namespace std;
 
 class HashTable{
     public:
+        HashTable(); //constructor
+
+        void put(); //add element
+
+        void get(); //get element
+
+        void outputHashTable(); //print out all elements in hash
 
     private:
+        void hashing(); //hashing function
 };
 
 
