c2cb43f21c376ba58682db018d5aff1226e08d7a
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2023-11-28 21:40:48 -0500

    check and fix all edge related material before moving on

--- C++/Assignment4/Edge.cpp
+++ C++/Assignment4/Edge.cpp
@@ -2,6 +2,8 @@
 //For use with weighted and directed graphs
 #include "Edge.hpp"
 
-Edge::Edge(Vertex* to, Vertex* from, int cost){
-
+Edge::Edge(Vertex* to, Vertex* from, int cost){ //edge class constructor
+    this->to = to;
+    this->from = from;
+    this->cost = cost;
 }
